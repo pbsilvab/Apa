3262abada9bce691be4350030a23f111b790b095
pedro <pbsilvab@gmail.com>
2019-05-05 16:00:53 -0300

    front template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             
         <!-- Styles -->
         <style>
@@ -21,73 +23,72 @@
                 height: 100vh;
                 margin: 0;
             }
-
-            .full-height {
-                height:50vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-            .text-center{
-                 text-align: center;
-            }
-
-            .title {
-                font-size: 48px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .position-ref{
+                color:#282828;
+                text-align:right;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('login') }}">Login</a>
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel">
+                            <h4 class="text-center">Web Developer</h4>
+                            <h5>Frontend</h5>
+                            <ul>
+                                <li>Angular</li>
+                                <li>VueJs</li>
+                            </ul>
+                            <h5>Backend</h5>
+                            <ul>
+                                <li>Laravel</li>
+                                <li>Go</li>
+                            </ul>
 
-                        @if (Route::has('register'))
-                            <a href="{{ url('register') }}">Register</a>
-                        @endif
-                    @endauth
+                            <hr>
+                            <h4 class="text-center">Web Services</h4>
+                            <ul>
+                                <li>Desarrollo de paginas web</li>
+                                <li>Desarrollo de Apps Moviles</li>
+                            </ul>
+
+                            
+
+                            <h3 class="social-icons text-center">
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-facebook-square"></i>
+                                <i class="fa fa-mail-bulk"></i>
+                            </h3>
+                        </div>
+                    </div>
                 </div>
-            @endif
-            <h1 class="title">&nbsp; Pedro Silva </h1>
-        </div>
-            <div class="block text-center">
-                <h4>COMING SOON</h4>
             </div>
+        </div>            
+        </div>
+        <div class="wraper2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                         <div class="block text-center">
+                             <h1 class="title">&nbsp; Pedro Silva </h1>
+                             <h5 class="subtitle">&nbsp; COMING SOON</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+
+                        </div>
+                </div>
+            </div>
+        </div>
     </body>
     <script src="{{asset('public/js/app.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
