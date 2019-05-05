d91fcb64c8ac1260e2cfb69b10e4d773f5124038
pedro <pbsilvab@gmail.com>
2019-05-05 12:28:31 -0300

    ¨Master¨

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{mix('css/app.css')}}">
+        <link rel="stylesheet" href="{{mix('public/css/app.css')}}">
             
         <!-- Styles -->
         <style>
